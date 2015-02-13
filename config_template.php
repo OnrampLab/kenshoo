c14081a23efd5b9aac7f1573b898c46d99c59a4e
turtlemt <turtlemt@gmail.com>
2015-02-13 16:20:43 +0800

    Update config_tempalte.php

--- config_template.php
+++ config_template.php
@@ -5,9 +5,13 @@ define('APPLICATION_FTP_USERNAME', 'username' );
 define('APPLICATION_FTP_USERPASS', 'password' );
 
 //System path
-define('APPLICATION_UPLOAD_DIR', '/path/to/upload' );
-define('APPLICATION_BACKUP_DIR', '/path/to/backup' );
+define('APPLICATION_UPLOAD_DIR', 'upload' );
+define('APPLICATION_BACKUP_DIR', 'upload/backup' );
 
 //Google document sheet 
 define('APPLICATION_GOOGLE_KENSHOO_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxx' );
 define('APPLICATION_GOOGLE_KENSHOO_GID', '0' );
+
+//Facebook
+define('APPLICATION_FACEBOOK_ID', '' );
+define('APPLICATION_FACEBOOK_SECRET', '' );
