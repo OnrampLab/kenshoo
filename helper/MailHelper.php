3fb7c1d1e2666589750963d7c9105cb54313e903
turtlemt <turtlemt@gmail.com>
2015-02-17 16:45:50 +0800

    Update mail message

--- helper/MailHelper.php
+++ helper/MailHelper.php
@@ -7,21 +7,21 @@ class MailHelper
     public static function sendStart()
     {
         $subject = '[auto] kenshoo start -'. date("Y-m-d H:i:s");
-        $body    = "kenshoo email by Simply Bridal\n" . $_SERVER['HOSTNAME'];
+        $body    = "kenshoo email by Simply Bridal";
         self::send($subject, $body);
     }
 
     public static function sendSuccess()
     {
         $subject = '[auto] kenshoo success -'. date("Y-m-d H:i:s");
-        $body    = "kenshoo email by Simply Bridal\n" . $_SERVER['HOSTNAME'];
+        $body    = "kenshoo email by Simply Bridal";
         self::send($subject, $body);
     }
 
     public static function sendFail()
     {
         $subject = '[auto] kenshoo fail -'. date("Y-m-d H:i:s");
-        $body    = "kenshoo email by Simply Bridal\n" . $_SERVER['HOSTNAME'];
+        $body    = "kenshoo email by Simply Bridal";
         self::send($subject, $body);
     }
 
@@ -32,7 +32,7 @@ class MailHelper
     private static function send($subject, $body)
     {
         $mail = new Message;
-        $mail->setFrom('office training2 <localhost@localhost.com>')
+        $mail->setFrom('Debug Message <localhost@localhost.com>')
             //->addTo('lawrence@lngmgmt.com')
             //->addTo('Davidc@lngmgmt.com')
             //->addTo('chris.tou@simplybridal.com')
