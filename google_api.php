c28b54dfe0f03b60981716497c19c2d52f71fff3
root <root@training3.training3>
2015-04-29 00:38:12 -0700

    Clean invisible control characters and unused code points

--- google_api.php
+++ google_api.php
@@ -129,6 +129,7 @@ function upgradeGoogleSheet()
         $row = $sheet->getRow($i);
 
         // 無論如何都必須修改的值
+        $row = filterUnusedCode($row);
         $row = updateDate($row);
 
         // 在 Override = 1 的情況要下修改的值
@@ -156,6 +157,29 @@ function upgradeGoogleSheet()
     }
     echo "\n";
 
+}
+
+/**
+ *  Clean invisible control characters and unused code points
+ *
+ *  \p{C} or \p{Other}: invisible control characters and unused code points.
+ *      \p{Cc} or \p{Control}: an ASCII 0x00–0x1F or Latin-1 0x80–0x9F control character.
+ *      \p{Cf} or \p{Format}: invisible formatting indicator.
+ *      \p{Co} or \p{Private_Use}: any code point reserved for private use.
+ *      \p{Cs} or \p{Surrogate}: one half of a surrogate pair in UTF-16 encoding.
+ *      \p{Cn} or \p{Unassigned}: any code point to which no character has been assigned.
+ *
+ *  該程式可以清除 RIGHT-TO-LEFT MARK (200F)
+ *
+ *  @see http://www.regular-expressions.info/unicode.html
+ *
+ */
+function filterUnusedCode( $row )
+{
+    foreach ( $row as $index => $value ) {
+        $row[$index] = preg_replace('/\p{C}+/u', "", $value );
+    }
+    return $row;
 }
 
 /**
